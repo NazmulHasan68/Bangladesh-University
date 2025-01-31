e5783fc5632f82aafe2699c62d6064093906310b
NazmulHasan68 <aishadowzone@gmail.com>
2025-01-31 17:10:32 +0600

    some change in package

--- frontend/src/components/pages Component/Apply.component/ApplyContact.jsx
+++ frontend/src/components/pages Component/Apply.component/ApplyContact.jsx
@@ -9,7 +9,7 @@ export default function ApplyContact() {
       </div>
       <div>
         <h1 className='text-md md:text-2xl font-semibold text-sky-950'>Contact Information for Assistance</h1>
-        <p className='text-xs md:text-md text-sky-900 py-1'>If you encounter any difficulties, please contact us at  <span className='font-semibold'>admission@bu.edu.bd  </span>or  <span className='font-semibold'>+88 01755-559301, +88 01755-559320</span> <br/> You can also click here to submit detail information</p>
+        <p className='text-xs md:text-md text-sky-900 py-1'>If you encounter any difficulties , please contact us at  <span className='font-semibold'>admission@bu.edu.bd  </span>or  <span className='font-semibold'>+88 01755-559301, +88 01755-559320</span> <br/> You can also click here to submit detail information</p>
       </div>
     </div>
   )
