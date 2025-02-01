14c3665501df1afe81760b902a1260f44548bd4c
NazmulHasan68 <aishadowzone@gmail.com>
2025-02-01 16:04:30 +0600

    FIX  club bug

--- frontend/src/components/pages Component/ClubComponent/ClubMainPart.jsx
+++ frontend/src/components/pages Component/ClubComponent/ClubMainPart.jsx
@@ -21,18 +21,17 @@ export default function ClubMainPart() {
         <select 
           id="club-select" 
           className="mt-1 basis-4/6 p-2 border rounded-md w-full md:w-auto"
-          onChange={handleClubChange} // Handle selection change
-          defaultValue=""
+          
         >
-          <option value="" disabled>Select a club</option>
-          <option value="/club/cricket">Cricket</option>
-          <option value="/club/football">Football</option>
-          <option value="/club/cultural-club">Cultural Club</option>
-          <option value="/club/programming-club">Programming Club</option>
-          <option value="/club/language-club">Language Club</option>
-          <option value="/club/debating-club">Debating Club</option>
-          <option value="/club/badminton">Badminton</option>
-          <option value="/club/carrom-board">Carrom Board</option>
+          <option  disabled>Select a club</option>
+          <option>Cricket</option>
+          <option >Football</option>
+          <option >Cultural Club</option>
+          <option >Programming Club</option>
+          <option >Language Club</option>
+          <option >Debating Club</option>
+          <option >Badminton</option>
+          <option >Carrom Board</option>
         </select>
       </div>
 
