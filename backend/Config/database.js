d8dbefedbfb88d5eb5459fefdad19503a61d6ee8
NazmulHasan68 <aishadowzone@gmail.com>
2025-02-14 13:59:34 +0600

    mongodb check

--- backend/Config/database.js
+++ backend/Config/database.js
@@ -4,6 +4,7 @@ export const ConnectDB = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected`);
+    res.send('MongoDb is connected');
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
